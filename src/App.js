a9d14c163b11e5fa9ed8f17f7d13005699e7ba0c
julia <julichis@mail.ru>
2023-04-28 17:27:33 +0400

    html structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="layout">
+      <section className="layout__header">
+        <div className="my-account">
+          <div className="my-account__info">
+            <h2 className="my-account__title">My balance</h2>
+            <div className="my-account__balance">$921.48</div>
+          </div>
+          <div className="my-account__logo"><img src={logo} alt="logo" /></div>
+        </div>
+      </section>
+      <section className="layout__body">
+        <div className="chart">
+          <h1 className="chart__title">Spending - Last 7 days</h1>
+          <div className="chart__body">
+
+          </div>
+        </div>
+        <div className="statistics">
+          <div className="statistics__absolute">
+            <h2 className="statistics__absolute-title">Total this month</h2>
+            <div className="statistics__absolute-value">$478.33</div>
+          </div>
+          <div className="statistics__relative">
+            <div className="statistics__relative-value">+2.4%</div>
+            <h2 className="statistics__relative-title">from last month</h2>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
