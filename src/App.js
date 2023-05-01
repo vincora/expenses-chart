d24cc6b9b02556b313943c364d7aebff7c904889
julia <julichis@mail.ru>
2023-05-01 17:36:38 +0400

    mobile version

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from "./logo.svg";
 import "./App.scss";
-import data from './data.json';
+import data from "./data.json";
+import cn from "classnames";
 
-const max = data.reduce((acc,cur) => {
-  return Math.max(acc, cur.amount)
- }, 0) ;
+const max = data.reduce((acc, cur) => {
+  return Math.max(acc, cur.amount);
+}, 0);
+const today = new Date();
+const currentDay = today.getDay();
+const week = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
 
 function App() {
   return (
@@ -15,22 +19,32 @@ function App() {
             <h2 className="my-account__title">My balance</h2>
             <div className="my-account__balance">$921.48</div>
           </div>
-          <div className="my-account__logo"><img src={logo} alt="logo" /></div>
+          <div className="my-account__logo">
+            <img src={logo} alt="logo" />
+          </div>
         </div>
       </section>
       <section className="layout__block">
         <div className="chart">
           <h1 className="chart__title">Spending - Last 7 days</h1>
-          <div className="chart__body" >
-            {data.map((item)=> {
-              const height = (item.amount/max)*100;
-              return(
-              <div className="chart__item" >
-                <div className="chart__bar" style={{"height": height + "%"}} ></div>
-                <div className="chart__day">{item.day}</div>
-              </div>)
+          <ul className="chart__body">
+            {data.map((item) => {
+              const height = (item.amount / max) * 100;
+              return (
+                <li className="chart__item" key={item.day}>
+                  <div
+                    className={cn("chart__bar", {
+                      chart__bar_mod: item.day === week[currentDay],
+                    })}
+                    style={{ height: height + "%" }}
+                  >
+                    <div className="chart__bar-info">{"$"+item.amount}</div>
+                  </div>
+                  <div className="chart__day">{item.day}</div>
+                </li>
+              );
             })}
-          </div>
+          </ul>
         </div>
         <div className="divider"></div>
         <div className="statistics">
